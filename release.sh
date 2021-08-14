020855be081324d09268b521d4ccff380cea8ae7
codeallthethingz <howdoicontactwill@gmail.com>
2021-08-14 13:52:32 -0700

    fiddling

--- release.sh
+++ release.sh
@@ -1,10 +1,15 @@
 #! /bin/sh
 
 if [[ "$1" == "" ]]; then
-    echo "usage: ./release.sh v0.0.1"
+    echo "usage: ./release.sh 0.0.1"
     echo "current releases: "
     git fetch --tags origin 
     exit 1
 fi
 
-gh release create "$1" --notes "" 
+mvn versions:set -DnewVersion="$1"
+rm pom.xml.versionsBackup
+git add pom.xml
+git commit -m "version bump $1 [skip ci]"
+git push
+gh release create "v$1" --notes "" 
