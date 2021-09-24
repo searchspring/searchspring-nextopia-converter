987a2650fd95b8214eefd5fa55ad8fc019458940
codeallthethingz <howdoicontactwill@gmail.com>
2021-09-24 11:38:27 -0700

    added scafolding for query translation

--- src/main/java/com/searchspring/nextopia/Converter.java
+++ src/main/java/com/searchspring/nextopia/Converter.java
@@ -1,20 +1,32 @@
 package com.searchspring.nextopia;
 
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLDecoder;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import com.google.gson.Gson;
 import com.google.gson.internal.LinkedTreeMap;
+import com.searchspring.nextopia.model.ParameterMappings;
 import com.searchspring.nextopia.model.SearchspringResponse;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.CompactWriter;
 
-public class Converter {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class Converter {
+    final Logger logger = LoggerFactory.getLogger(Converter.class);
+
+    private final String siteId;
     private final Gson GSON = new Gson();
     private final XStream xs = new XStream();
 
-    public Converter() {
+    public Converter(String siteId) {
+        this.siteId = siteId;
         xs.registerConverter(new MapEntryConverter());
         xs.alias("result", LinkedTreeMap.class);
     }
@@ -53,8 +65,37 @@ public class Converter {
         return "0";
     }
 
-    public String convertNextopiaQueryUrl(String nextopiaQueryUrl) {
-        return "newUrl";
+    public String convertNextopiaQueryUrl(String nextopiaQueryUrl) throws URISyntaxException {
+        URI uri = new URI(nextopiaQueryUrl);
+        Map<String, String> queryMap = parseQueryString(uri.getQuery());
+        StringBuilder sb = new StringBuilder("http://").append(siteId).append(".a.searchspring.io/api?siteId=").append(siteId);
+        mapParameter(sb, queryMap, ParameterMappings.NX_KEYWORDS, ParameterMappings.SS_KEYWORDS);
+        logger.debug("Converted {} to {}", nextopiaQueryUrl, sb.toString());
+        return sb.toString();
+    }
+
+    private void mapParameter(StringBuilder sb, Map<String, String> queryMap, String sourceParameter,
+            String destinationParameter) {
+        if (queryMap.containsKey(sourceParameter)) {
+            sb.append("&");
+            sb.append(destinationParameter).append("=").append(queryMap.get(sourceParameter));
+        }
+    }
+
+    private Map<String, String> parseQueryString(String query) {
+        Map<String, String> queryMap = new LinkedHashMap<String, String>();
+        String[] pairs = query.split("&");
+        for (String pair : pairs) {
+            int idx = pair.indexOf("=");
+            try {
+                queryMap.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"),
+                        URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                // Should never happen.
+                logger.error("Missing utf-8 encoding", e);
+            }
+        }
+        return queryMap;
     }
 
 }
