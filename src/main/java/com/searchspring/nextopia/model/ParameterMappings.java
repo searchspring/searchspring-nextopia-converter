6f2a1afab4b77a9fb6c56a3c53365810723ee95a
codeallthethingz <howdoicontactwill@gmail.com>
2021-10-10 06:09:47 -0700

    query -> q

--- src/main/java/com/searchspring/nextopia/model/ParameterMappings.java
+++ src/main/java/com/searchspring/nextopia/model/ParameterMappings.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ParameterMappings {
-    public final static String SS_KEYWORDS = "query";
+    public final static String SS_KEYWORDS = "q";
     public final static String SS_SITE_ID = "siteId";
 
     public final static String NX_KEYWORDS = "keywords";
--- src/test/java/com/searchspring/nextopia/ConverterTest.java
+++ src/test/java/com/searchspring/nextopia/ConverterTest.java
@@ -1,7 +1,7 @@
 package com.searchspring.nextopia;
 
 import static org.junit.Assert.assertEquals;
-
+import static com.searchspring.nextopia.model.ParameterMappings.*;
 import java.net.URISyntaxException;
 
 import javax.xml.transform.Source;
@@ -48,24 +48,27 @@ public class ConverterTest {
 
     @Test
     public void ConvertQueryKeywordTest() throws URISyntaxException {
-        assertEquals("https://abcd12.a.searchspring.io/api?siteId=" + SITE_ID + "&query=b%C3%B6b",
+        assertEquals("https://abcd12.a.searchspring.io/api?siteId=" + SITE_ID + "&" + SS_KEYWORDS + "=b%C3%B6b",
                 converter.convertNextopiaQueryUrl(
                         "https://ecommerce-search.nextopiasoftware.com/return-results.php?keywords=böb&page=1&json=1&client_id=b9d7aa2736f88c2a410dde6f66b946b5&ip=50.92.124.92&user_agent=Mozilla%2F5.0+%28Macintosh%3B+Intel+Mac+OS+X+10_15_7%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F93.0.4577.82+Safari%2F537.36"));
     }
 
     @Test
     public void ConvertQueryRefineTest() throws URISyntaxException {
-        //                                                          siteId=c2sajm&filter.panini_press_type_id7741124012283341841=Smooth%20Plates%7C_%7C7741124012284472342&experimentalMode=discrete-strict&debug=1&resultsPerPage=36&backend=saluki&preview=1
-        assertEquals("https://abcd12.a.searchspring.io/api?siteId=" + SITE_ID + "&query=world+fork&filter.Flatwaretypeid7741124012283339335=Dinner+Fork",
+        assertEquals(
+                "https://abcd12.a.searchspring.io/api?siteId=" + SITE_ID + "&" + SS_KEYWORDS
+                        + "=world+fork&filter.Flatwaretypeid7741124012283339335=Dinner+Fork",
                 converter.convertNextopiaQueryUrl(
                         "https://api.nextopiasoftware.com/return-results.php?xml=1&client_id=66141eeeacafe959b288238d65b176cb&keywords=world+fork&refine=y&return_single_refines=1:1&Flatwaretypeid7741124012283339335=Dinner+Fork&refines_mode=keep:Flatwaretypeid7741124012283339335&ip=10.3.62.24&page=1&res_per_page=20&searchtype=0&requested_fields=Sku"));
     }
+
     @Test
     public void ConvertQueryRefineOrTest() throws URISyntaxException {
-       // TODO 
+        // TODO
     }
+
     @Test
     public void ConvertQueryRefineAndTest() throws URISyntaxException {
-       // TODO 
+        // TODO
     }
 }
